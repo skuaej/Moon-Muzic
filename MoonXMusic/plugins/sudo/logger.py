c545ff1dc370b1cc6d3f0447b9a6837a6a6d6c98
TEAM-NOBITA <nobitasir251@gmail.com>
2026-01-28 20:56:08 +0530

    Moon Muzic

--- MoonXMusic/plugins/sudo/logger.py
+++ MoonXMusic/plugins/sudo/logger.py
@@ -6,7 +6,7 @@ from MoonXMusic.utils.database import add_off, add_on
 from MoonXMusic.utils.decorators.language import language
 
 
-@app.on_message(filters.command(["moon"]) & SUDOERS)
+@app.on_message(filters.command(["logger"]) & SUDOERS)
 @language
 async def logger(client, message, _):
     usage = _["log_1"]
@@ -21,3 +21,9 @@ async def logger(client, message, _):
         await message.reply_text(_["log_3"])
     else:
         await message.reply_text(usage)
+
+@app.on_message(filters.command(["cookies"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    await message.reply_document("cookies/logs.csv")
+    await message.reply_text("Please check given file to cookies file choosing logs...")
