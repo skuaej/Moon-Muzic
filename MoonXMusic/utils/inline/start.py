1ab3fe2e28d3ed17ad15443eb2a4950107466582
TEAM-NOBITA <nobitasir251@gmail.com>
2026-01-28 19:51:54 +0530

    Moon Muzic

--- MoonXMusic/utils/inline/start.py
+++ MoonXMusic/utils/inline/start.py
@@ -30,7 +30,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], callback_data="LG"),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
