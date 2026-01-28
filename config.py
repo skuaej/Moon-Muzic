20c4cf7ecbec22331a7228728ea76f0b7fabd9be
TEAM-NOBITA <nobitasir251@gmail.com>
2026-01-28 19:24:02 +0530

    Moon Muzic

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TEAM-NOBITA/leharmusic",
+    "https://github.com/TEAM-NOBITA/Moon-Muzic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
