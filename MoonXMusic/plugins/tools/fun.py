ffbdb73e50f3f2b0a4dee1f3a9057182d87eb2b9
skuaej <sumankumar821310@gmail.com>
2026-02-12 10:48:00 +0530

    Create fun.py

---
+++ MoonXMusic/plugins/tools/fun.py
@@ -0,0 +1,114 @@
+# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
+# Location: Supaul, Bihar
+#
+# All rights reserved.
+#
+# This code is the intellectual property of Nand Yaduwanshi.
+# You are not allowed to copy, modify, redistribute, or use this
+# code for commercial or personal projects without explicit permission.
+#
+# Allowed:
+# - Forking for personal learning
+# - Submitting improvements via pull requests
+#
+# Not Allowed:
+# - Claiming this code as your own
+# - Re-uploading without credit or permission
+# - Selling or using commercially
+#
+# Contact for permissions:
+# Email: badboy809075@gmail.com
+
+
+import requests
+from pyrogram import filters
+from pyrogram.types import Message
+
+from MoonXMusic import app
+
+
+@app.on_message(
+    filters.command(
+        [
+            "dice",
+            "ludo",
+            "dart",
+            "basket",
+            "basketball",
+            "football",
+            "slot",
+            "bowling",
+            "jackpot",
+        ]
+    )
+)
+async def dice(c, m: Message):
+    command = m.text.split()[0]
+    if command == "/dice" or command == "/ludo":
+
+        value = await c.send_dice(m.chat.id, reply_to_message_id=m.id)
+        await value.reply_text("ʏᴏᴜʀ sᴄᴏʀᴇ ɪs {0}".format(value.dice.value))
+
+    elif command == "/dart":
+
+        value = await c.send_dice(m.chat.id, emoji="🎯", reply_to_message_id=m.id)
+        await value.reply_text("ʏᴏᴜʀ sᴄᴏʀᴇ ɪs {0}".format(value.dice.value))
+
+    elif command == "/basket" or command == "/basketball":
+        basket = await c.send_dice(m.chat.id, emoji="🏀", reply_to_message_id=m.id)
+        await basket.reply_text("ʏᴏᴜʀ sᴄᴏʀᴇ ɪs {0}".format(basket.dice.value))
+
+    elif command == "/football":
+        value = await c.send_dice(m.chat.id, emoji="⚽", reply_to_message_id=m.id)
+        await value.reply_text("ʏᴏᴜʀ sᴄᴏʀᴇ ɪs {0}".format(value.dice.value))
+
+    elif command == "/slot" or command == "/jackpot":
+        value = await c.send_dice(m.chat.id, emoji="🎰", reply_to_message_id=m.id)
+        await value.reply_text("ʏᴏᴜʀ sᴄᴏʀᴇ ɪs {0}".format(value.dice.value))
+    elif command == "/bowling":
+        value = await c.send_dice(m.chat.id, emoji="🎳", reply_to_message_id=m.id)
+        await value.reply_text("ʏᴏᴜʀ sᴄᴏʀᴇ ɪs {0}".format(value.dice.value))
+
+
+bored_api_url = "https://apis.scrimba.com/bored/api/activity"
+
+
+@app.on_message(filters.command("bored", prefixes="/"))
+async def bored_command(client, message):
+    response = requests.get(bored_api_url)
+    if response.status_code == 200:
+        data = response.json()
+        activity = data.get("activity")
+        if activity:
+            await message.reply(f"𝗙𝗲𝗲𝗹𝗶𝗻𝗴 𝗯𝗼𝗿𝗲𝗱? 𝗛𝗼𝘄 𝗮𝗯𝗼𝘂𝘁:\n\n {activity}")
+        else:
+            await message.reply("Nᴏ ᴀᴄᴛɪᴠɪᴛʏ ғᴏᴜɴᴅ.")
+    else:
+        await message.reply("Fᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴀᴄᴛɪᴠɪᴛʏ.")
+
+
+__MODULE__ = "Fᴜɴ"
+__HELP__ = """
+**ʜᴀᴠɪɴɢ ꜰᴜɴ:**
+
+• `/dice`: Rᴏʟʟs ᴀ ᴅɪᴄᴇ.
+• `/ludo`: Pʟᴀʏ Lᴜᴅᴏ.
+• `/dart`: Tʜʀᴏᴡs ᴀ ᴅᴀʀᴛ.
+• `/basket` ᴏʀ `/basketball`: Pʟᴀʏs ʙᴀsᴋᴇᴛʙᴀʟʟ.
+• `/football`: Pʟᴀʏs ғᴏᴏᴛʙᴀʟʟ.
+• `/slot` ᴏʀ `/jackpot`: Pʟᴀʏs ᴊᴀᴄᴋᴘᴏᴛ.
+• `/bowling`: Pʟᴀʏs ʙᴏᴡʟɪɴɢ.
+• `/bored`: Gᴇᴛs ʀᴀɴᴅᴏᴍ ᴀᴄᴛɪᴠɪᴛʏ ɪғ ʏᴏᴜ'ʀᴇ ʙᴏʀᴇᴅ.
+"""
+
+
+# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
+
+
+# ❤️ Love From ShrutiBots
